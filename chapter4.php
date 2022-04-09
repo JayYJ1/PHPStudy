2b573d5a1779609d97590fd1a54c4fd0ac1f603f
JayYJ1 <tntnqk12@gmail.com>
2022-04-09 23:53:24 +0900

    0409

--- chapter4.php
+++ chapter4.php
@@ -98,3 +98,92 @@ echo"<html>
    //  var_dump($result1);
 echo $num3."~".$num4."정수 중".$num5."의 배수 합계 : ".$result1;
 ?>
+<?php
+        echo"<html>
+        <head>
+            <h2>수목원 입장료 구하기</h2>
+        </head>
+            <body>
+                <form method='POST' action='chapter4.php'>
+                 구분:  <select name='time'>
+                <option value='일반'>일반</option>
+                <option value='할인대상'>할인대상</option>
+              </select>
+              개인/단체 :  <select name='pg'>
+              <option value='개인'>개인</option>
+              <option value='단체'>단체</option>
+            </select>
+              나이: <input type='number' name='age' value='0' />
+                <input type='submit' name='submit'/>
+                </form>
+            </body>
+        </html>";
+
+     $time=$_POST['time'];
+     $pg=$_POST['pg'];
+     $age=$_POST['age'];
+    function paytable1($time, $age, $pg){
+        $fee=0;
+        if($time=="일반"){//일반 손님
+            if($pg=="개인")//일반손님-개인
+            {
+                if($age<8){
+                    $fee=6000;
+                }
+                elseif($age>7 and $age<=18){
+                    $fee=7000;
+                }
+                else{
+                    $fee=9500;
+                }
+            }
+            else{ //일반손님-단체
+                if($age<8){
+                    $fee=5000;
+                }
+                elseif($age>7 and $age<=18){
+                    $fee=6000;
+                }
+                else{
+                    $fee=8500;
+                }
+            }
+        }
+        else{ //할인대상
+            if($pg=="개인")//할인대상-개인
+            {
+                if($age<8){
+                    $fee=5000;
+                }
+                elseif($age>7 and $age<=18){
+                    $fee=6000;
+                }
+                else{
+                    $fee=8500;
+                }
+            }
+            else{//할인대상-단체
+                if($age<8){
+                    $fee=4000;
+                }
+                elseif($age>7 and $age<=18){
+                    $fee=5000;
+                }
+                else{
+                    $fee=7500;
+                }
+            }
+        }
+        return $fee;
+    }
+    $result=paytable1($time,$age,$pg);
+    echo "구분 : " . $time ."<br>"."개인/단체: ".$pg. "<br>" ."나이 : " . $age ."세"."<br>" ."요금 : ". number_format($result) ."원<br>";
+?>
+<?php
+    echo"<h2>1차원 배열 출력</h2>";
+    $bae = array(1,1,2,3,5,8,13,21,34,55,89);
+    $c=count($bae);
+    for($i=0; $i<$c; $i++){
+        echo $i+1,"번 = ".$bae[$i]."<br>";
+    }
+?>
