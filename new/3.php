d4517af55f8cfedc0590f75ecc1a4c2db386cf57
JayYJ1 <102240669+JayYJ1@users.noreply.github.com>
2022-04-27 18:15:17 +0900

    Update 3.php

--- new/3.php
+++ new/3.php
@@ -9,6 +9,17 @@ echo "<br>";
 $b = "welcome home!";
 $change = str_replace("home","school",$b);
 echo $change;
+echo "<br>";
 // 글자 개수 : 12
 // welcome school!
+
+$cut = substr($b,0,7);
+$cut2 = substr($b,-5,7);
+echo $cut;
+echo "<br>";
+echo $cut2;
+// 글자 개수 : 12
+// welcome school!
+// welcome
+// home!
 ?>
