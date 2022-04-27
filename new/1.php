c28a8ff6a3239db4d89bfcfae1505a59bfb267e0
JayYJ1 <102240669+JayYJ1@users.noreply.github.com>
2022-04-27 18:04:07 +0900

    Update 1.php

--- new/1.php
+++ new/1.php
@@ -1,1 +1,14 @@
+<?php
+  
+  $str = " 공백 지우기 ";
+  echo '|' . trim($str) . '|';
+  echo "<br>";
 
+$str1 = " 공백 지우기 ";
+  echo '|' . ltrim($str1) . '|';
+  echo "<br>";
+
+$str2 = " 공백 지우기 ";
+  echo '|' . rtrim($str2) . '|';
+  echo "<br>";
+?>
